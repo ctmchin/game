6f8d7a20cd8f2f39c302fba2226a1abe67f022f1
ctmchin <jimchanchin@gmail.com>
2026-09-04 09:19:30 +0800

    Add export for grammar.js in index.js

--- data/index.js
+++ data/index.js
@@ -2,4 +2,5 @@
 // It imports from all the other files in this folder and re-exports them.
 // This makes it much easier to import our data later!
 
-export * from './idioms.js'
+export * from './idioms.js';
+export * from './grammar.js'
