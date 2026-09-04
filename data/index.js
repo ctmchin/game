299aec6fac509a4cc6fff60d0a08b6cb4ae22318
ctmchin <jimchanchin@gmail.com>
2026-09-04 08:55:16 +0800

    Update and rename README.md to data/index.js

---
+++ data/index.js
@@ -0,0 +1,5 @@
+// This file acts as a central hub for all our data files.
+// It imports from all the other files in this folder and re-exports them.
+// This makes it much easier to import our data later!
+
+export * from './idioms.js'
