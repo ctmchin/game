4b4106455a281a13ead5bcf61af41c2db129f2e4
ctmchin <jimchanchin@gmail.com>
2026-09-05 23:22:15 +0800

    Update main.js

--- modules/main.js
+++ modules/main.js
@@ -1,6 +1,7 @@
 import './firebase.js';
 import './auth.js';
 import './ui.js';
-import './quiz-engine.js'; // <-- This line is very important.
+import './quiz-engine.js';
+import './profile.js'; // <-- ADD THIS LINE
 
 console.log("Main controller has loaded all systems.");
