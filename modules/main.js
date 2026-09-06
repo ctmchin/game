de8257bd134bfbf182242844085fcbe4bebfc996
ctmchin <jimchanchin@gmail.com>
2026-09-06 12:06:05 +0800

    Comment out quiz-engine import in main.js

--- modules/main.js
+++ modules/main.js
@@ -3,7 +3,7 @@
 // ========================================================
 import './firebase.js';
 import './profile.js';      // <-- Turn on the profile system
-import './quiz-engine.js';   // <-- Turn on the quiz system
+// import './quiz-engine.js';   // <-- Turn on the quiz system
 import './ui.js';
 import './auth.js';          // <-- Auth is last, as it depends on the others
 
