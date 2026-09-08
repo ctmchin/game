23f90413e196981a1149fee0984b2cca279d9aa7
ctmchin <jimchanchin@gmail.com>
2026-09-08 18:08:04 +0800

    Update articles.js

--- data/articles.js
+++ data/articles.js
@@ -1,7 +1,7 @@
 // data/articles.js
-import { articles_part1 } from './articles-part1.js';
-import { articles_part2 } from './articles-part2.js';
-import { articles_part3 } from './articles-part3.js';
+import {articles_part1} from './articles-part1.js';
+import {articles_part2} from './articles-part2.js';
+import {articles_part3} from './articles-part3.js';
 
 export const articlesData = [
   ...articles_part1,
