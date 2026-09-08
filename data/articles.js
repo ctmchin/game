0820136ee249c10546f01da08816013f8d59079f
ctmchin <jimchanchin@gmail.com>
2026-09-08 17:52:38 +0800

    Create articles.js

---
+++ data/articles.js
@@ -0,0 +1,10 @@
+// data/articles.js
+import { articles_part1 } from './articles-part1.js';
+import { articles_part2 } from './articles-part2.js';
+import { articles_part3 } from './articles-part3.js';
+
+export const articlesData = [
+  ...articles_part1,
+  ...articles_part2,
+  ...articles_part3
+];
